e4f7bf8b7f79af31cb25b311510756e60c8138e2
savvieSammie <aderibigbe.s.adekunle@gmail.com>
2023-06-05 00:53:25 +0100

    changed the models to include product category

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.dom.minidom import Document
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from . models import Category, Product
 
-admin.site.register(Category)
 
-admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug' : ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
 
 #sammie, aderibigbe 
